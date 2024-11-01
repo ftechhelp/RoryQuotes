476603e05379e73f379777b7b23e90a14790dbae
Vincent Fontaine <ftechhelp@gmail.com>
2024-11-01 14:48:32 -0700

    good base structure

--- obj/Debug/net8.0/RoryQuotes.AssemblyInfo.cs
+++ obj/Debug/net8.0/RoryQuotes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoryQuotes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8e2eb9fdd7a93150a53b3e8bc57b37877b3264a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoryQuotes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoryQuotes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
